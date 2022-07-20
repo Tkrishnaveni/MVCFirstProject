3c3048e77f66f697b3d1ae56c28d1c1cafd6d8f4
Tkrishnaveni <45869263+Tkrishnaveni@users.noreply.github.com>
2022-07-20 03:27:04 -0700

    Add files via upload

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,25 @@
+﻿using MVCFirstProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCFirstProject.Controllers
+{
+    public class EmployeeController :Controller
+    {
+        // GET: Employee
+        public ActionResult EmployeeInfo()
+        {
+           Employee employee = new Employee()
+            {
+                EmployeeId = 215266,
+                EmployeeName = "Krishnaveni",
+                EmployeeLocation = "Chennai"
+
+            };
+            return View(employee);
+        }
+    }
+}
